e8db1e9e35778f26ac2a6fc4b396fee72b1285fa
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2020-05-01 18:24:25 -0500

    Solcione el problema en que los autos no se guardaban y solo se escribian utilizando arrays

--- js_basic/reto_objects/objetos.js
+++ js_basic/reto_objects/objetos.js
@@ -6,8 +6,8 @@ function car(brand, model, year) {
 var brand_array = ["Tesla", "Lamborghini", "Ford", "Chevrolet", "Hyundai", "Tesla", "Audi"];
 var model_array = ["Model S", "Aventador SVJ", "Explorer", "Optra", "Tucson", "Model 3", "R8"];
 var year_array = [2020, 2018, 2019, 2010, 2008, 2020, 2018];
-
+var new_car = [];
 for(var i = 0; brand_array.length > i; i++){
-    var new_car = new car(brand_array[i], model_array[i], year_array[i]);
+    new_car.push(new car(brand_array[i], model_array[i], year_array[i]));
     console.log(new_car);
 }
