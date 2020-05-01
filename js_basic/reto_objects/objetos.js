79037e21270247bca9e6df1e6abcff34d79ef865
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2020-05-01 18:11:35 -0500

    Realice el reto de creación de autos tipo objeto automatizado

---
+++ js_basic/reto_objects/objetos.js
@@ -0,0 +1,13 @@
+function car(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+}
+var brand_array = ["Tesla", "Lamborghini", "Ford", "Chevrolet", "Hyundai", "Tesla", "Audi"];
+var model_array = ["Model S", "Aventador SVJ", "Explorer", "Optra", "Tucson", "Model 3", "R8"];
+var year_array = [2020, 2018, 2019, 2010, 2008, 2020, 2018];
+
+for(var i = 0; brand_array.length > i; i++){
+    var new_car = new car(brand_array[i], model_array[i], year_array[i]);
+    console.log(new_car);
+}
