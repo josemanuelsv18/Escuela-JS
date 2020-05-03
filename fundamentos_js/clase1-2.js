977621d5234f5c14ecc06d131d150aa43d3fd5b2
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2020-05-03 18:59:08 -0500

    commit de las primeras tres clases de fundamentos

---
+++ fundamentos_js/clase1-2.js
@@ -0,0 +1,14 @@
+var nombre = 'Josemanuel';
+var apellido = 'Sifontes';
+
+var nombre_mayusculas = nombre.toUpperCase();
+var apellido_minusculas = apellido.toLowerCase();
+
+var primera_letra_nombre = nombre.charAt(0);
+var cantidad_letras_nombre = nombre.length;
+
+var nombre_completo = `${nombre} ${apellido.toUpperCase()}`;
+
+var str = nombre.substr(1, 2);
+
+var reto = nombre.charAt((cantidad_letras_nombre - 1));
---
+++ fundamentos_js/clase3.js
@@ -0,0 +1,25 @@
+var edad = 18;
+
+edad += 1;
+
+var peso = 75;
+
+peso -= 2
+
+var sandwich = 1;
+
+peso = peso + sandwich;
+
+var jugar_futbol = -3;
+
+peso += jugar_futbol;
+
+var precio_vino = 200.3;
+
+var total = Math.round(precio_vino *100 *3) /100;
+var totalstr = total.toFixed(2);
+var total2 = parseFloat(totalstr);
+
+var pizza = 8;
+var persona = 2;
+var cantidad_porciones_personas = pizza / persona;
