32e8a1cfda7a3a2b59a35ac8293ef6b32ede705a
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2020-05-21 21:31:03 -0500

    iluminar la secuencia de los distintos niveles

--- fundamentos_js/simon.js
+++ fundamentos_js/simon.js
@@ -1,26 +1,57 @@
 const BTN_INICIO = document.getElementById('btnEmpezar');
-const CELESTE = document.getElementById('celeste');
-const VIOLETA = document.getElementById('violeta');
-const NARANJA = document.getElementById('naranja');
-const VERDE = document.getElementById('verde');
+const celeste = document.getElementById('celeste');
+const violeta = document.getElementById('violeta');
+const naranja = document.getElementById('naranja');
+const verde = document.getElementById('verde');
 
 class Juego{
     constructor(){
         this.inicializar();
         this.generarSecuencia();
+        this.siguienteNivel();
     }
     inicializar(){
         BTN_INICIO.classList.add('hide');
         this.nivel = 1;
         this.colores = {
-            celeste: CELESTE,
-            violeta: VIOLETA,
-            naranja: NARANJA,
-            verde: VERDE
+            celeste,
+            violeta,
+            naranja,
+            verde
         }
     }
     generarSecuencia(){
         this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+    }
+    siguienteNivel(){
+        this.iluminarSecuencia()
+    }
+
+    transformarNumAColor(numero){
+        switch(numero){
+            case 0:
+                return 'celeste'
+            case 1:
+                return 'violeta'
+            case 2:
+                return 'naranja'
+            case 3:
+                return 'verde'
+        }
+    }
+
+    iluminarSecuencia(){
+        for(let i = 0; i < this.nivel; i++){
+            const color = this.transformarNumAColor(this.secuencia[i]);
+            setTimeout(() => this.iluminarColor(color), 1000 * i)
+        }
+    }
+    iluminarColor(color){
+        this.colores[color].classList.add('light');
+        setTimeout(() => this.apagarColor(color), 350)
+    }
+    apagarColor(color){
+        this.colores[color].classList.remove('light');
     }
 }
 
