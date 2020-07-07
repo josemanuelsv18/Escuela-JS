07141bbd9ee752128cb004b90bad67bd408efaa8
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2020-07-06 20:06:16 -0500

    terminada la clase 7 de ES6+

---
+++ ECMAScript+6/src/es7/index.js
@@ -0,0 +1,15 @@
+//Includes para los arrays
+let numbers = [1, 2, 3 , 7, 8];
+
+if(numbers.includes(9)){
+    console.log('Si se incluye el numero 7');
+}else {
+    console.log('el numero 7 no se encuentra');
+}
+
+//potencia
+let base = 4;
+let exponente = 3;
+let result = base ** exponent //con el doble asterisco puedes realizar la 
+//operación de potencia
+console.log(result);
