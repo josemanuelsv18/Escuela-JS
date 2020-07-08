3a83a76b654e3516fe655b38abff9c4ac53b3d9c
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2020-07-07 21:49:56 -0500

    finaliza la clase 8 de ECMAScript 6+

--- ECMAScript+6/src/es8/index.js
+++ ECMAScript+6/src/es8/index.js
@@ -28,3 +28,28 @@ const string = 'hello';
 console.log(string.padStart(7, 'hi')); //expected hihello
 console.log(string.padEnd(12, ' -----')); //expected hello -----
 
+//Async Await
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (false)
+            ? setTimeout(() => resolve('Hello world'), 3000)
+            : reject(new Error('Test Error'))
+    });
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch(error){
+        console.log(error);
+    }
+};
+anotherFunction();
