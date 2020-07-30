94cc102bed4a54c3417a6b5a32f93102ab5b4797
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2020-07-30 11:33:03 -0500

    se agrego un archivo separado para el codigo en javascript

---
+++ pro_js/index.js
@@ -0,0 +1,16 @@
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+
+function MediaPlayer(config){
+    this.media = config.el;
+}
+MediaPlayer.prototype.playPause = function() {
+    if(this.media.paused){
+        this.media.play();
+    }else{
+        this.media.pause()
+    }
+};
+const player = new MediaPlayer({el: video});
+
+button.onclick = () => player.playPause();
